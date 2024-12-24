e24e3dc70405856c4a4bbf40b769f9d4f870a003
SupakornEiEi <Supakorn254617@gmail.com>
2024-12-25 02:50:40 +0700

    add bar

--- firstpush/urls.py
+++ firstpush/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Homepage ,name='Home'),
     path('plot/', views.plot_view, name='plot'),
+    path('plotbar/', views.plotbar_view, name='plotbar'),
 ]
--- firstpush/views.py
+++ firstpush/views.py
@@ -22,3 +22,18 @@ def plot_view(request):
 
     return HttpResponse(buffer.getvalue(), content_type='image/png')
 
+def plotbar_view(request):
+    fig, ax = plt.subplots()
+    x = ['A', 'B', 'C', 'D', 'E']
+    y = [7, 8, 4, 20, 17]
+    ax.bar(x, y)  # เปลี่ยนจาก ax.plot เป็น ax.bar
+
+    buffer = BytesIO()
+    plt.savefig(buffer, format='png')
+    plt.close(fig)
+    buffer.seek(0)
+
+    return HttpResponse(buffer.getvalue(), content_type='image/png')
+
+
+
